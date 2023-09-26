3fab9a01a04f3fc29ef9b11de7f96ed05c8e4d1d
Marvin Morales <marvin@thinknow.com>
2023-09-25 21:42:32 -0500

    Cambios agregados PostgreSQL

--- runner.sh
+++ runner.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-uvicorn app.main:app --reload
+
+# Instala Pipenv si no está instalado
+pip install pipenv
+
+# Crea un entorno virtual con Pipenv y activa el entorno
+pipenv install --deploy
+pipenv shell
+
+# Ejecuta tu servidor
+pipenv run server
+# uvicorn app.main:app --reload
